91127d4a374f922eb6a518c099ac51920730ead7
Derya <deryakendircikahraman@gmail.com>
2025-08-27 00:18:11 -0700

    Fix Jest configuration and add basic tests

---
+++ __tests__/basic.test.js
@@ -0,0 +1,13 @@
+describe('Basic Test', () => {
+  it('should pass a simple test', () => {
+    expect(true).toBe(true)
+  })
+
+  it('should add numbers correctly', () => {
+    expect(1 + 1).toBe(2)
+  })
+
+  it('should handle strings', () => {
+    expect('hello').toBe('hello')
+  })
+}) 
